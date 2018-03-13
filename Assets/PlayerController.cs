e1ba389405eb2e166dc3e844ed3776ad15f6fbf7
djjbanx-laptop <jrh395@drexel.edu>
2018-03-13 12:47:33 -0400

    Revert "Revert "Initial Commit""
    This reverts commit fd81737e92b560799f5f267fd41dff33931d05ab.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+
+		float x = Input.GetAxis("Horizontal") * 0.1f;
+		float y = Input.GetAxis ("Vertical") * 0.1f;
+		transform.Translate (x, y, 0);
+	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponent<MeshRenderer> ().material.color = Color.red;
+	}
+}
