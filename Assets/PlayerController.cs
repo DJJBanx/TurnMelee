e11945ef77c5724981c378130125fa87713f91ce
Dennis George <djg365@drexel.edu>
2018-03-13 13:11:36 -0400

    Added wrap around movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System;
 
 public class PlayerController : NetworkBehaviour {
 
@@ -19,6 +20,14 @@ public class PlayerController : NetworkBehaviour {
 		float x = Input.GetAxis("Horizontal") * 0.1f;
 		float y = Input.GetAxis ("Vertical") * 0.1f;
 		transform.Translate (x, y, 0);
+
+		if (Math.Abs (transform.position.x) > 20) {
+			print ("above or below 10");
+			transform.Translate (-2 * transform.position.x, 0, 0);
+		}
+		if (Math.Abs (transform.position.y) > 10) {
+			transform.Translate (0, -2 * transform.position.y, 0);
+		}
 	}
 
 	public override void OnStartLocalPlayer() {
